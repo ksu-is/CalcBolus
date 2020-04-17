e33f7d8094139333f1172488cc59918d236185a8
lizsmoot <asmoot5@students.kennesaw.edu>
2020-04-17 17:32:09 -0400

    CalcBolus Commit #3
    For this most recent commit I worked on building the program more by adding if/elif/else statements and while to the necessary user input strings for two reasons. The first being to add a pathway for the input to be placed into the initialized variables and the second being to make sure the input the user is giving is valid. I added the remaining variables that needed to be initizalized in the second half of the program, and finished the first round of decision trees for all of the yes/no questions without yet adding the code for calculating any of the numbers, which will begin with my next commit. Lastly, I edited some of the wording ad added some "\n" spacing for legibility.

--- calcbolus.py
+++ calcbolus.py
@@ -1,13 +1,19 @@
-#initialize variables
+#initialize correction factor variables
 lowfact = 0
 highfact = 0
+corfactnum = 0
 
 #introduce the CalcBolus program
 print("\nWelcome to CalcBolus! Please consult with your doctor before using this program.\n")
 
 #get user input for blood glucose correction factor and ask if default physical correction factors need to be changed. 
 print("Please input your blood glucose correction factor number as determined by your doctor. For example, if you divide your current number minus your target number by 35, please enter 35.")
-corfact = int(input("BG Correction Factor: "))
+while corfactnum == 0:
+  corfact = input("BG Correction Factor: ")
+  if corfact.isnumeric():
+    corfactnum = int(corfact)
+  else:
+    print("Invalid input. Please enter a numeric value.")
 
 print("\nThe current default correction factor for hypoglycemia-causing events (like exercise) is .8 . Would you like to change this number?")
 while lowfact == 0:
@@ -17,7 +23,7 @@ while lowfact == 0:
   elif changelow.lower() == 'n':
     lowfact = 0.8
   else:
-    print("That is not a valid input. Please enter 'y' to change, or enter 'n' to keep.")
+    print("Invalid input. Please enter 'y' to change, or enter 'n' to keep.")
 
 print("\nThe current default correction factor for hyperglycemia-causing events like illness is 1.25 . Would you like to change this number?")
 while highfact == 0:
@@ -27,20 +33,70 @@ while highfact == 0:
   elif changehigh.lower() == 'n':
     highfact = 1.25
   else: 
-    print("That is not a valid input. Please enter 'y' to change, or enter 'n' to keep.")
+    print("Invalid input. Please enter 'y' to change, or enter 'n' to keep.")
 
+#initialize other variables for second round of user input
+bgnum = 0
+carbsnum = 0
+exercise = 0 
+alcintake = 0
+illness = 0
+meds = 0
 
 #get user input on current blood glucose, carb intake, and phyiscal factors
-bg = int(input("\nPlease enter your latest blood glucose reading: "))
-carbs = int(input("Please enter your carbohydrate count for your meal: "))
+while bgnum == 0:
+  bg = input("\nPlease enter your latest blood glucose reading: ")
+  if bg.isnumeric():
+    bgnum = int(bg) 
+  else:
+     print("Invalid input. Please enter a numeric value.")
+
+while carbsnum = 0:
+  carbs = input("\nPlease enter the carbohydrate count for your meal: ")
+  if carbs.isnumeric():
+    carbsnum = int(carbs)
+  else:
+     print("Invalid input. Please enter a numeric value.")
+
 print("\nFor the following questions, please enter 'y' for yes, or 'n' for no.")
-exercise = input("Will you be doing, or have you done, any strenuous or prolonged phyiscal exercise today?: ")
-alcintake = input("Will you have, or have you had, more than two low-carb alcoholic bevarages today?: ")
-illness = input("Are you currently sick with a cold, the flu, or another phyiscla illness that cause hyperglycemic trends?: ")
-meds = input("Are you currently taking any medications, like corticosteroids or blood pressure medication, that cause hyperglycemic trends?: ")
+while exercise == 0:
+  exercise = input("\nWill you be doing, or have you done, any strenuous or prolonged phyiscal exercise today?: ")
+  if exercise.lower() == 'y':
+    exercise = 1
+  elif exercise.lower() == 'n':
+    exercise = 2
+  else:
+    print("Invalid input. Please enter either 'y' or 'n'.")
+
+while alcintake == 0:
+  alcintake = input("\nWill you have, or have you had, more than two low-carb alcoholic bevarages today?: ")
+  if alcintake.lower() == 'y':
+    alcintake = 1
+  elif alcintake.lower() == 'n':
+    alcintake = 2
+  else:
+    print("Invalid input. Please enter either 'y' or 'n'.")
+
+while illness == 0: 
+  illness = input("\nAre you currently sick with a cold, the flu, or another phyiscla illness that cause hyperglycemic trends?: ")
+   if illness.lower() == 'y':
+    illness = 1
+  elif illness.lower() == 'n':
+    illness = 2
+  else:
+    print("Invalid input. Please enter either 'y' or 'n'.")
+
+while meds == 0:
+  meds = input("\nAre you currently taking any medications, like corticosteroids or blood pressure medication, that cause hyperglycemic trends?: ")
+   if meds.lower() == 'y':
+    meds = 1
+  elif meds.lower() == 'n':
+    meds = 2
+  else:
+    print("Invalid input. Please enter either 'y' or 'n'.")
 
 
-#Read Inputs
+#Calculate insulin doasge
 while (forward == "no"):
   product = str(input("Name of product: "))
 
@@ -62,10 +118,10 @@ while (forward == "no"):
     
 #Calculate
 
-while 
+#while 
 x = 0
 
-while 
+#while 
 
 print ("\n")
 print("--------------------------------")
