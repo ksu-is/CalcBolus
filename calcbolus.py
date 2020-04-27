cb75c4a8ecac4b1ae3c19555a5227c90b3096dcf
lizsmoot <asmoot5@students.kennesaw.edu>
2020-04-27 15:34:31 -0400

    Commit #7
    Added colorama module to the program to show text in different colors. Added text colors to the first half of the program, pre-calculation. Next commit will be adding red text to error messages and adding text color to the calculations part and the final output.

--- calcbolus.py
+++ calcbolus.py
@@ -1,31 +1,36 @@
+#import colorama module
+from colorama import init
+
 #initialize correction factor, target BG, and ICR variables
 cfnum = 0
 targetnum = 0
 icrnum = 0
 
 #introduce the CalcBolus program
-print("\nWelcome to CalcBolus! Please consult with your doctor before using this program.\n")
+from colorama import Fore, Back, Style
+print(Fore.GREEN + "Welcome to CalcBolus! Please consult with your doctor before using this program.\n")
+print(Style.RESET_ALL)
 
 #get user input for blood glucose correction factor, target blood glucose, and insulin to carb ratio (ICR) 
-print("Please input your blood glucose correction factor number as determined by your doctor. For example, if you divide your current number minus your target number by 35, please enter 35.")
+print(Fore.YELLOW + "Please input your blood glucose correction factor number as determined by your doctor. For example, if you divide your current number minus your target number by 35, please enter 35.")
 while cfnum == 0:
-  corfact = input("BG Correction Factor: ")
+  corfact = input(Fore.LIGHTCYAN_EX + "BG Correction Factor: ")
   if corfact.isnumeric():
     cfnum = int(corfact)
   else:
     print("Invalid input. Please enter a numeric value.")
 
-print("\nPlease input your target blood glucose number. ")
+print(Fore.YELLOW + "\nPlease input your target blood glucose number. ")
 while targetnum == 0:
-  target = input("Target blood glucose: ")
+  target = input(Fore.LIGHTCYAN_EX + "Target blood glucose: ")
   if target.isnumeric():
     targetnum = int(target)
   else:
     print("Invalid input. Please enter a numeric value.")
 
-print("\n Please enter your insulin to carb ratio. For example, if you take one unit for every 5 carbs, please enter 5.")
+print(Fore.YELLOW + "\n Please enter your insulin to carb ratio. For example, if you take one unit for every 5 carbs, please enter 5.")
 while icrnum == 0:
-  icr = input("insulin to carb ratio: ")
+  icr = input(Fore.LIGHTCYAN_EX + "insulin to carb ratio: ")
   if icr.isnumeric():
     icrnum = int(icr)
   else:
@@ -36,21 +41,21 @@ while icrnum == 0:
 lowfact = 0
 highfact = 0
 
-print("\nThe current default correction factor for hypoglycemia-causing events (like exercise) is .8 . Would you like to change this number?")
+print(Fore.YELLOW + "\nThe current default correction factor for hypoglycemia-causing events (like exercise) is .8 . Would you like to change this number?")
 while lowfact == 0:
-  changelow = input("Enter 'y' to change, or enter 'n' to keep: ")
+  changelow = input(Fore.MAGENTA + "Enter 'y' to change, or enter 'n' to keep: ")
   if changelow.lower() == 'y':
-    lowfact = float(input("Enter your new correction factor for hypoglycemia-causing events as a decimal such as .5 or .92: "))
+    lowfact = float(input(Fore.LIGHTCYAN_EX + "Enter your new correction factor for hypoglycemia-causing events as a decimal such as .5 or .92: "))
   elif changelow.lower() == 'n':
     lowfact = 0.8
   else:
     print("Invalid input. Please enter 'y' to change, or enter 'n' to keep.")
 
-print("\nThe current default correction factor for hyperglycemia-causing events like illness is 1.25 . Would you like to change this number?")
+print(Fore.YELLOW + "\nThe current default correction factor for hyperglycemia-causing events like illness is 1.25 . Would you like to change this number?")
 while highfact == 0:
-  changehigh = input("Enter 'y to change, or enter 'n' to keep: ")
+  changehigh = input(Fore.MAGENTA + "Enter 'y to change, or enter 'n' to keep: ")
   if changehigh.lower() == 'y':
-    highfact = float(input("Enter your new correction factor for hyperglycemia-causing events as a decimal such as 1.35 or 1.1: "))
+    highfact = float(input(Fore.LIGHTCYAN_EX + "Enter your new correction factor for hyperglycemia-causing events as a decimal such as 1.35 or 1.1: "))
   elif changehigh.lower() == 'n':
     highfact = 1.25
   else: 
@@ -66,22 +71,22 @@ meds = 0
 
 #get user input on current blood glucose, carb intake, and phyiscal factors
 while bgnum == 0:
-  bg = input("\nPlease enter your latest blood glucose reading: ")
+  bg = input(Fore.LIGHTCYAN_EX + "\nPlease enter your latest blood glucose reading: ")
   if bg.isnumeric():
     bgnum = int(bg) 
   else:
      print("Invalid input. Please enter a numeric value.")
 
 while chonum == 0:
-  cho = input("\nPlease enter the carbohydrate count for your meal: ")
+  cho = input(Fore.LIGHTCYAN_EX + "\nPlease enter the carbohydrate count for your meal: ")
   if cho.isnumeric():
     chonum = int(cho)
   else:
      print("Invalid input. Please enter a numeric value.")
 
-print("\nFor the following questions, please enter 'y' for yes, or 'n' for no.")
+print(Fore.YELLOW + "\nFor the following questions, please enter 'y' for yes, or 'n' for no.")
 while exercise == 0:
-  exercise = input("\nWill you be doing, or have you done, any strenuous or prolonged phyiscal exercise today?: ")
+  exercise = input(Fore.LIGHTCYAN_EX + "\nWill you be doing, or have you done, any strenuous or prolonged phyiscal exercise today?: ")
   if exercise.lower() == 'y':
     exercise = 1
   elif exercise.lower() == 'n':
@@ -90,7 +95,7 @@ while exercise == 0:
     print("Invalid input. Please enter either 'y' or 'n'.")
 
 while alcintake == 0:
-  alcintake = input("\nWill you have, or have you had, more than two low-carb alcoholic bevarages today?: ")
+  alcintake = input(Fore.LIGHTCYAN_EX + "\nWill you have, or have you had, more than two low-carb alcoholic bevarages today?: ")
   if alcintake.lower() == 'y':
     alcintake = 1
   elif alcintake.lower() == 'n':
@@ -99,16 +104,16 @@ while alcintake == 0:
     print("Invalid input. Please enter either 'y' or 'n'.")
 
 while illness == 0: 
-  illness = input("\nAre you currently sick with a cold, the flu, or another phyiscla illness that cause hyperglycemic trends?: ")
+  illness = input(Fore.LIGHTCYAN_EX +  "\nAre you currently sick with a cold, the flu, or another phyiscla illness that cause hyperglycemic trends?: ")
   if illness.lower() == 'y':
     illness = 1
   elif illness.lower() == 'n':
     illness = 2
   else:
-    print("Invalid input. Please enter either 'y' or 'n'.")
+    print(Fore.LIGHTCYAN_EX + "Invalid input. Please enter either 'y' or 'n'.")
 
 while meds == 0:
-  meds = input("\nAre you currently taking any medications, like corticosteroids or blood pressure medication, that cause hyperglycemic trends?: ")
+  meds = input(Fore.LIGHTCYAN_EX + "\nAre you currently taking any medications, like corticosteroids or blood pressure medication, that cause hyperglycemic trends?: ")
   if meds.lower() == 'y':
     meds = 1
   elif meds.lower() == 'n':
@@ -160,17 +165,4 @@ while bolus == 0:
     print("Sorry, the program cannot read your output. Please restart the program and try again.")
 
 
-      
 
-    
-
-      
-
-
-
-#Calculate insulin doasge
-
-
-#print ("\n")
-#print("--------------------------------")
-#print ("Total:                        "   + str(total))
